bf2290db23dcb559b09259911cf77df91cdf246a
Ana Ceprnja <aceprnja@iun.edu>
2021-01-26 13:41:36 -0700

    images added

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
 /* const items go here
 crib, score, and yoda */
-
+const cribs = document.querySelectorAll(".crib");
+const yodas = document.querySelectorAll (".yoda");
+const points = document.querySelector(".score");
 // set timer 
 
 // pick random crib for yoda to pop out of
