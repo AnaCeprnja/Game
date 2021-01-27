a2e5d3d3c082bddd9313ed80d2a0c3a0a088e5cc
Ana Ceprnja <aceprnja@iun.edu>
2021-01-27 16:55:37 -0700

    java start

--- game.js
+++ game.js
@@ -6,5 +6,14 @@ crib, score, and yoda */
 // pick random crib for yoda to pop out of
 //holes node list
 
+let cribs = document.querySelectorAll('.crib'),
+amountOfPoints = document.querySelector('.score'),
+yodas = document.querySelectorAll('.yoda');
 
+if (x <= 10) {
+  console.log("you win!");
+}else {
+  console.log("You lost!");
+}
+}
 
