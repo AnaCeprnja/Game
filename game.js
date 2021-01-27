70c18649e876093733d2d7fa12e437403eb37528
Ana Ceprnja <aceprnja@iun.edu>
2021-01-27 16:37:15 -0700

    fixed

--- game.js
+++ game.js
@@ -1,11 +1,10 @@
 /* const items go here
 crib, score, and yoda */
-const cribs = document.querySelectorAll(".crib");
-const yodas = document.querySelectorAll (".yoda");
-const points = document.querySelector(".score");
+
 // set timer 
 
 // pick random crib for yoda to pop out of
 //holes node list
 
-//function to give points when mole is clicked
+
+
